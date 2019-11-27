5973f508694b22b8b0b888d8acea04928938b71f
TLambrecht1988 <thomas.lambrecht2@student.howest.be>
2019-11-27 15:30:07 +0100

    test 1

--- ClassLibrary1.Lib/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ClassLibrary1.Lib
-{
-    public class Class1
-    {
-    }
-}
---
+++ ClassLibrary1.Lib/Zoo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo.Lib
+{
+    class Zoo
+    {
+        public decimal EntranceFee { get; set; } = 50.0M;
+
+        public Zoo (decimal entrancefee)
+        {
+            EntranceFee = entrancefee;
+
+        }
+
+    }
+}
