d8991513e4cabc3d8d970e253329db9417b73a62
TLambrecht1988 <thomas.lambrecht2@student.howest.be>
2019-11-27 15:35:45 +0100

    commit extra

--- ClassLibrary1.Lib/Zoo.cs
+++ ClassLibrary1.Lib/Zoo.cs
@@ -7,6 +7,7 @@ namespace Zoo.Lib
     public class Zoo
     {
         public decimal EntranceFee { get; set; } = 50.0M;
+        public LionCage lionCage { get; set; }
 
         public Zoo (decimal entrancefee)
         {
